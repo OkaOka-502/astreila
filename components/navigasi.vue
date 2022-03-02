98cf444801560ba17df15819f956a88f47dd96ff
OkaOka-502 <ciciokta819@gmail.com>
2022-03-02 16:21:04 +0700

    Modifikasi nav responsive

--- components/navigasi.vue
+++ components/navigasi.vue
@@ -1,23 +1,67 @@
 <template>
-  <div>
+  <div class="container">
     <div class="row navv">
       <div class="col-md-12">
-        <nav class="navbar bg-danger" style="height: 150%">
-          <div class="nav justify-content">
-            <NuxtLink class="ml-2 mr-4 text-white" to="/">Home</NuxtLink>
-            <NuxtLink class="mr-4 text-white" to="Menu">Menu</NuxtLink>
-            <NuxtLink class="text-white" to="Contact" style="margin-right: 50em"
-              >Contact</NuxtLink
-            >
+        <nav
+          class="navbar navbar-expand-lg navbar-dark bg-danger"
+          style="height: 150%"
+        >
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <ul class="navbar-nav mt-2 mt-lg-0" style="margin-right: 45em">
+              <li class="nav-item active">
+                <NuxtLink class="ml-2 mr-4 text-white ml-4" to="/"
+                  >Home</NuxtLink
+                >
+              </li>
+              <li class="nav-item active">
+                <NuxtLink class="ml-2 mr-4 text-white" to="Menu">Menu</NuxtLink>
+              </li>
+              <li class="nav-item active">
+                <NuxtLink class="ml-2 mr-4 text-white" to="Contact"
+                  >Contact</NuxtLink
+                >
+              </li>
+            </ul>
             <a href="https://api.whatsapp.com/send?phone=6282116442464"
               ><Iconw
             /></a>
-
-            <!-- <a href="" class="mr-3"><Iconf /></a>
-            <a href="" class="mr-3"><Icontele /></a> -->
           </div>
         </nav>
       </div>
     </div>
   </div>
 </template>
+
+
+  // <div class="container">
+  //   <div class="row navv">
+  //     <div class="col-md-12">
+  //       <nav class="navbar bg-danger responsive" style="height: 150%">
+  //         <div class="nav justify-content">
+  //           <NuxtLink class="ml-2 mr-4 text-white" to="/">Home</NuxtLink>
+  //           <NuxtLink class="mr-4 text-white" to="Menu">Menu</NuxtLink>
+  //           <NuxtLink class="text-white" to="Contact" style="margin-right: 50em"
+  //             >Contact</NuxtLink
+  //           >
+  //           <a href="https://api.whatsapp.com/send?phone=6282116442464"
+  //             ><Iconw
+  //           /></a>
+
+  //           <!-- <a href="" class="mr-3"><Iconf /></a>
+  //           <a href="" class="mr-3"><Icontele /></a> -->
+  //         </div>
+  //       </nav>
+  //     </div>
+  //   </div>
+  // </div>
--- pages/coba.vue
+++ pages/coba.vue
@@ -1,12 +1,44 @@
 <template>
-  <div>
-    <div class="jumbotron jumbotron-fluid">
-      <h1 class="display-4">Fluid jumbotron</h1>
-      <p class="lead">
-        This is a modified jumbotron that occupies the entire horizontal space
-        of its parent.
-      </p>
+  <div class="container">
+    <div class="row navv">
+      <div class="col-md-12">
+        <nav
+          class="navbar navbar-expand-lg navbar-dark bg-danger"
+          style="height: 150%"
+        >
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <ul class="navbar-nav mt-2 mt-lg-0" style="margin-right: 47em">
+              <li class="nav-item active">
+                <NuxtLink class="ml-2 mr-4 text-white ml-4" to="/"
+                  >Home</NuxtLink
+                >
+              </li>
+              <li class="nav-item active">
+                <NuxtLink class="ml-2 mr-4 text-white" to="Menu">Menu</NuxtLink>
+              </li>
+              <li class="nav-item active">
+                <NuxtLink class="ml-2 mr-4 text-white" to="Contact"
+                  >Contact</NuxtLink
+                >
+              </li>
+            </ul>
+            <a href="https://api.whatsapp.com/send?phone=6282116442464"
+              ><Iconw
+            /></a>
+          </div>
+        </nav>
+      </div>
     </div>
-    <Footer class="mt-0" />
   </div>
 </template>
